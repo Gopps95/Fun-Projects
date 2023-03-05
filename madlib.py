ac74bc11917010e3e1923b2a34960455953f6df6
gopps95 <shankargopi7@gmail.com>
2023-03-05 21:47:33 +0530

    Added

---
+++ madlib.py
@@ -0,0 +1,6 @@
+verb4 = input("Enter the adjective")
+verb5 = input("verb")
+verb1 = input("verb")
+verb2 = input("verb")
+madlib = f"The {verb4} doesn't {verb5}, it's the vibe you put into it. If the {verb1} sounds {verb2}, music sounds good."
+print(madlib)
