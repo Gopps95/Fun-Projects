46cd6e99e82261a0740092c29f8a3095ac2aca35
gopps95 <shankargopi7@gmail.com>
2023-03-08 23:26:47 +0530

    partial-done

--- hangman.py
+++ hangman.py
@@ -19,16 +19,21 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    user_letter = input('Guess a letter:').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0 :
+        print('You have used these letters: ',' '.join(used_letters))
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ',' '.join(word_list))
 
-    elif user_letter in used_letters:
-        print('You have already used the character.')
-    else:
-        print('Invalid')
+        user_letter = input('Guess a letter:').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
 
+        elif user_letter in used_letters:
+            print('You have already used the character.')
+        else:
+            print('Invalid')
 
-user_input = input("Type Something")
+hangman()
+#user_input = input("Type Something")
