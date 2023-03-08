8260688422938ccba0b75e316e9ceb8b4dd955f3
gopps95 <shankargopi7@gmail.com>
2023-03-08 20:43:44 +0530

    took user input

--- hangman.py
+++ hangman.py
@@ -1,7 +1,34 @@
 import json
+import random
+import string 
 
 
 f = open('words.json')
 data = json.load(f)
 
-print(data)
+def get_valid_word(data):
+    while '-' in word or ' ' in word:
+        word = random.choice(data)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(data)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    user_letter = input('Guess a letter:').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already used the character.')
+    else:
+        print('Invalid')
+
+
+user_input = input("Type Something")
